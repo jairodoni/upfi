aa9fd9c92841bddb74e78d044172ea06d1bdd8f6
Jairo Doni <jairo.doni97@gmail.com>
2021-07-26 20:03:08 -0300

    Desafio concluído 🎉

--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -7,6 +7,7 @@ import {
   Image,
   Link,
 } from '@chakra-ui/react';
+import { Card } from '../Card';
 
 interface ModalViewImageProps {
   isOpen: boolean;
@@ -19,5 +20,37 @@ export function ModalViewImage({
   onClose,
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
+
+  const handleCloseModal = (): void => {
+    onClose();
+  };
+
   // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+  return (
+
+    <Modal isOpen={isOpen} onClose={handleCloseModal} isCentered size="4xl">
+      <ModalOverlay />
+      <ModalContent
+        mx="auto"
+        w="auto"
+        h="auto"
+        maxW={['300px', '500px', '900px']}
+        maxh={['350px', '450px', '600px']}
+      >
+        <ModalBody p={0}>
+          <Image
+            src={imgUrl}
+            maxW={['300px', '500px', '900px']}
+            maxh={['350px', '450px', '600px']}
+            objectFit="cover"
+          />
+        </ModalBody>
+        <ModalFooter bgColor="pGray.800" h="2rem" py="20px" borderBottomRadius="5px">
+          <Link href={imgUrl} isExternal fontSize="1rem" mr="auto">
+            Abrir original
+          </Link>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  )
 }
